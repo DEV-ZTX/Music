9e28974cb0755008ce3831d0d06e5b8f4752953f
DEV-ZTX <sucidaldazai777@gmail.com>
2025-03-25 09:39:42 +0530

    Update restart.py

--- AnonXMusic/plugins/sudo/restart.py
+++ AnonXMusic/plugins/sudo/restart.py
@@ -33,64 +33,75 @@ async def log_(client, message, _):
 @app.on_message(filters.command(["update", "gitpull"]) & SUDOERS)
 @language
 async def update_(client, message, _):
-    response = await message.reply_text("Checking for updates...")
-
+    if await is_heroku():
+        if HAPP is None:
+            return await message.reply_text(_["server_2"])
+    response = await message.reply_text(_["server_3"])
     try:
-        repo = Repo()  # Load the Git repository
+        repo = Repo()
+    except GitCommandError:
+        return await response.edit(_["server_4"])
     except InvalidGitRepositoryError:
-        return await response.edit("Error: This directory is not a valid Git repository.")
-    except GitCommandError as e:
-        return await response.edit(f"Git error: {str(e)}")
-
-    # Fetch updates from GitHub
-    os.system(f"git fetch origin {config.UPSTREAM_BRANCH}")
-    await asyncio.sleep(5)
-
-    # Count new commits
-    new_commits = list(repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"))
-    if not new_commits:
-        return await response.edit("No new updates available.")
-
-    # Generate update summary
+        return await response.edit(_["server_5"])
+    to_exc = f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null"
+    os.system(to_exc)
+    await asyncio.sleep(7)
+    verification = ""
+    REPO_ = repo.remotes.origin.url.split(".git")[0]
+    for checks in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
+        verification = str(checks.count())
+    if verification == "":
+        return await response.edit(_["server_6"])
     updates = ""
-    repo_url = repo.remotes.origin.url.split(".git")[0]
+    ordinal = lambda format: "%d%s" % (
+        format,
+        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
+    )
+    for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
+        updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
+    _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
+    _final_updates_ = _update_response_ + updates
+    if len(_final_updates_) > 4096:
+        url = await AnonyBin(updates)
+        nrs = await response.edit(
+            f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
+        )
+    else:
+        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
+    os.system("git stash &> /dev/null && git pull")
 
-    for commit in new_commits:
-        commit_date = datetime.fromtimestamp(commit.committed_date).strftime("%d %b %Y")
-        updates += f"<b>➣ Commit: <a href='{repo_url}/commit/{commit.hexsha}'>{commit.summary}</a> by {commit.author}</b>\n"
-        updates += f"  ➥ Date: {commit_date}\n\n"
-
-    # If updates are too long, upload to Pastebin
-    if len(updates) > 4000:
-        paste_url = await AnonyBin(updates)
-        updates = f"New updates available! Check here: <a href='{paste_url}'>View Changes</a>"
-
-    # Apply updates
-    await response.edit(f"<b>New updates found!</b>\n\n{updates}\n\nApplying updates...")
-    
-    # Stash uncommitted changes and pull latest code
-    os.system("git reset --hard")
-    os.system("git stash")
-    os.system(f"git pull origin {config.UPSTREAM_BRANCH}")
-
-    # Notify active chats about restart
     try:
-        active_chats = await get_active_chats()
-        for chat_id in active_chats:
+        served_chats = await get_active_chats()
+        for x in served_chats:
             try:
-                await app.send_message(chat_id=int(chat_id), text="The bot is updating and will restart soon.")
-                await remove_active_chat(chat_id)
-                await remove_active_video_chat(chat_id)
+                await app.send_message(
+                    chat_id=int(x),
+                    text=_["server_8"].format(app.mention),
+                )
+                await remove_active_chat(x)
+                await remove_active_video_chat(x)
             except:
                 pass
+        await response.edit(f"{nrs.text}\n\n{_['server_7']}")
     except:
         pass
 
-    # Restart the bot
-    await response.edit("Update applied! Restarting bot...")
-    os.system("pip3 install -r requirements.txt")
-    os.system(f"kill -9 {os.getpid()} && bash start")
-    exit()
+    if await is_heroku():
+        try:
+            os.system(
+                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
+)
+            return
+        except Exception as err:
+            await response.edit(f"{nrs.text}\n\n{_['server_9']}")
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=_["server_10"].format(err),
+            )
+    else:
+        os.system("pip3 install -r requirements.txt")
+        os.system(f"kill -9 {os.getpid()} && bash start")
+        exit()
 
 
 # Restart Bot
